cb046404a9a1e0a3d06089baaa9a23495008d182
rahul.yadav@asurion.com <rahul.yadav@asurion.com>
2019-07-16 23:32:26 -0500

    End of section 4

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from  'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  baseURL = 'http://localhost:5000/api/auth/';
+constructor(private httpClient: HttpClient) { }
+
+login(model: any) {
+  return this.httpClient.post(this.baseURL + 'login', model)
+  .pipe(
+    map((response: any) => {
+      const user = response;
+      if (user) {
+         localStorage.setItem('token', user.token);
+       }
+    })
+  );
+}
+
+register(model: any) {
+    return this.httpClient.post(this.baseURL + 'register', model);
+  }
+}
